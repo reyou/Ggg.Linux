19486a2762d13e4811655a3b384dad1dec6353d1
Teoman Shipahi <aytekoz@yahoo.com>
2018-04-30 23:33:42 -0400

    some commands added.

--- app/application.js
+++ app/application.js
@@ -1,7 +1,6 @@
 //=============================================================================
 // How do I search for available packages from the command-line?
 // https://askubuntu.com/questions/160897/how-do-i-search-for-available-packages-from-the-command-line
-var update = "sudo apt-get update";
 var search = "apt-cache search {keyword}";
 var apt_search = "apt search {package-name}";
 /**So finally we can sort by dictionary order usingsort -d and show only a
@@ -12,5 +11,5 @@ var sort_all = "apt-cache search . |sort -d |less";
 //=============================================================================
 // https://askubuntu.com/questions/17823/how-to-list-all-installed-packages
 // How to list all installed packages
-let apt_list = "apt list --installed"
+let apt_list = "apt list --installed";
 //=============================================================================
